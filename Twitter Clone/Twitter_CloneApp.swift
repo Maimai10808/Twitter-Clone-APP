56097a5f29b0af4afe333d5f4b74db4eee28b266
Maimai10808 <331686549@qq.com>
2025-03-20 00:17:18 +0800

    Initial Commit

---
+++ Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by mac on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
